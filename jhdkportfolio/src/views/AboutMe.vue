be2b7dcf36857d8616c753a404b18ebf2312b32c
Doomtower <jessehudak707@outlook.com>
2024-02-05 17:21:10 -0800

    added to about me

--- jhdkportfolio/src/views/AboutMe.vue
+++ jhdkportfolio/src/views/AboutMe.vue
@@ -3,25 +3,32 @@
     <div class="container py-4 text-left">
         <img class="portrait" src="@/assets/HeadshotCasual12142023.png" alt="jesse headshot"> 
       <p>
+        I am Jesse Hudak - A curious problem solver, rabbit hole spelunker, jack-of-all with a sharp wit and a penchant for conversation. 
+        <br>
         I am an avid learner, working into the world of tech. I have always
-        enjoyed building and repairing computers; coding seems like a good
-        logical step to use as a creative outlet. I also highly enjoy problem
-        solving, so coding is a fun, yet frustrating way to get more of that into
-        my day.
+        enjoyed building and repairing computers, however that was not the career path I would have chosen for fulfilling work. I wanted something with a creative outlet, rewarding, fun, challenging and something that I could do anywhere.
+        <br>
+        After joining the cohort through U.C Berkeley, I started to feel like this was the right path to get me to where I am going, albeit an arduous path. 
+        <br>
+        I have taken on several personal projects and I am working with some team-mates that I stuck with after the cohort was over, to work and develop some projects together. I am constantly on the lookout for open-source projects that I can contribute to, and doing drills and challenges to get my skills up.
       </p>
       <div class="container py-4 text-left">
       <ul style="list-style-type:circle;">
         <h3>Random hobbies and interests / Fun</h3>
         <li>Beach trips and tidepooling</li>
-        <li>Body Surfing </li>
-        <li>Succulents </li>
-        <li>Fungus </li>
-        <li>Spending hours on Google Maps looking at everything </li>
+        <li>Body Surfing</li>
+        <li>Working Out/ Hiking, have lost 40lbs since last April!</li>
+        <li>Record Collecting/ Vintage Audio Gear</li>
+        <li>Playing Guitar</li>
+        <li>Succulents</li>
+        <li>Fungus & Mycology</li>
+        <li>Reading, currently on a Sci-Fi kick</li>
+        <li>Spending hours on Google Maps looking at the world, finding interesting places to go or research </li>
       </ul>
       </div>
 
       <h4>
-        <p>Copyright 2023 @ Jesse Hudak | Made with Vue.js/Vite/TailwindCSS and VSCode</p>
+        <p>Copyright 2023 @ Jesse Hudak | Made with Vue.js/ Vite/ TailwindCSS & VSCode</p>
       </h4>
     </div>
   </div>
